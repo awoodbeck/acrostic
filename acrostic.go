6e6bedbaa22a19eaab5824ef9c9242dc0d01ba89
Adam Woodbeck <adam@woodbeck.net>
2016-03-17 16:37:06 -0400

    Refactored words a bit and added tests.

--- acrostic.go
+++ acrostic.go
@@ -1,3 +1,62 @@
 //go:generate go run -tags=dev assets_generate.go
 
 package acrostic
+
+import (
+	"bytes"
+	"crypto/rand"
+	"fmt"
+	"math"
+	"math/big"
+)
+
+// Acrostic maintains a list of adjectives and nouns, and returns an acrostical
+// phrase for a given word.
+type Acrostic struct {
+}
+
+// calcEntropy returns the calculated entropy where "t" is the total number of
+// items available for random selection, and "n" is the number of random
+// items selected.
+func calcEntropy(t, n float64) float64 {
+	return math.Log2(math.Pow(t, n))
+}
+
+// randomInt64 generates a random 64-bit integer from 0 to "max."
+func randomInt64(max int64) (n int64, err error) {
+	if max < 1 {
+		err = fmt.Errorf("The given max (%d) cannot be less than 1.", max)
+		return
+	}
+
+	var i *big.Int
+	i, err = rand.Int(rand.Reader, big.NewInt(max))
+	if err != nil {
+		return
+	}
+
+	n = i.Int64()
+
+	return
+}
+
+// stripCarriageReturns removes all carriage return characters
+// from the given byte slice and returns a new byte slice.
+func stripCarriageReturns(crb []byte) (b []byte, err error) {
+	if bytes.Index(crb, []byte{'\r'}) == -1 {
+		return crb, nil
+	}
+
+	bBuf := bytes.Buffer{}
+
+	for _, v := range bytes.Split(crb, []byte{'\r'}) {
+		_, err = bBuf.Write(v)
+		if err != nil {
+			return
+		}
+	}
+
+	b = bBuf.Bytes()
+
+	return
+}
--- assets.go
+++ assets.go
@@ -4,4 +4,5 @@ package acrostic
 
 import "net/http"
 
+// Assets implements file system access to the "assets" directory.
 var Assets http.FileSystem = http.Dir("assets")
