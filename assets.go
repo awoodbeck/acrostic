b0369686d81b4b12975e0d52d3506df96b03822e
Adam Woodbeck <adam@woodbeck.net>
2016-03-19 00:26:37 -0400

    Moved assets into a separate package.

--- assets.go
+++
@@ -1,8 +0,0 @@
-// +build dev
-
-package acrostic
-
-import "net/http"
-
-// Assets implements file system access to the "assets" directory.
-var Assets http.FileSystem = http.Dir("assets")
--- assets_generate.go
+++ assets_generate.go
@@ -6,15 +6,16 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/awoodbeck/acrostic"
+	"github.com/awoodbeck/acrostic/assets"
 	"github.com/shurcooL/vfsgen"
 )
 
 func main() {
-	err := vfsgen.Generate(acrostic.Assets, vfsgen.Options{
+	err := vfsgen.Generate(assets.Assets, vfsgen.Options{
 		BuildTags:    "!dev",
-		PackageName:  "acrostic",
+		PackageName:  "assets",
 		VariableName: "Assets",
+		Filename:     "assets/vfsgen.go",
 	})
 	if err != nil {
 		fmt.Println(err)
